65bd0da03533562a91dfa206ba1bb8ad029f7a3f
Alex Ko <ko.e.alexander@gmail.com>
2019-06-01 23:39:39 -0400

    added bottom navigation and fragments

--- app/src/main/java/ko/alex/bedbugko/Bot1Frag.java
+++ app/src/main/java/ko/alex/bedbugko/Bot1Frag.java
@@ -1,12 +1,27 @@
 package ko.alex.bedbugko;
 
+import android.os.CountDownTimer;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Bot1Frag extends Fragment {
+
+    TextView countdownText;
+    FloatingActionButton countdownFAB, submitDataFAB;
+
+    CountDownTimer countDownTimer;
+    long timeLeftMilliseconds = 300000; // 5 mins
+    boolean timerRunning;
+
+    ProgressBar progressBar;
 
     public Bot1Frag(){
         // Required empty public constructor
@@ -18,6 +33,9 @@ public class Bot1Frag extends Fragment {
 
         //Working with Firebase and Recyclerview
         //https://www.youtube.com/watch?v=kyGVgrLG3KU&list=LLj1xIyoM3IcZs9XdwFDaJWA&index=3&t=0s
+
+        //Simple Countdown Timer
+        //https://www.youtube.com/watch?v=zmjfAcnosS0
     }
 
     @Override
@@ -25,7 +43,65 @@ public class Bot1Frag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.bot1frag, container, false);
+
+        progressBar = view.findViewById(R.id.progressBar);
+
+        submitDataFAB = view.findViewById(R.id.submitDataFAB);
+        submitDataFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Data Submitted!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        countdownText = view.findViewById(R.id.countdownText);
+        countdownFAB = view.findViewById(R.id.countdownFAB);
+        countdownFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(timerRunning == true){
+                    countDownTimer.cancel();
+                    countdownFAB.setImageDrawable(getResources().getDrawable(R.drawable.playicon));
+                    timerRunning = false;
+                } else{
+                    countDownTimer = new CountDownTimer(timeLeftMilliseconds, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            timeLeftMilliseconds = millisUntilFinished;
+                            updateTimer();
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            //something something
+                        }
+                    }.start();
+                    countdownFAB.setImageDrawable(getResources().getDrawable(R.drawable.pauseicon));
+                    timerRunning = true;
+                }
+            }
+        });
+
         return view;
     } //END ONCREATEVIEW
+
+    public void updateTimer(){
+        //http://www.androidtutorialshub.com/android-count-down-timer-tutorial/
+        int minutes = (int) (timeLeftMilliseconds/60000); // Divide by 60 seconds
+        int seconds = (int) (timeLeftMilliseconds % 60000 / 1000);
+        String timeLeftText;
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if(seconds < 10) timeLeftText += "0";
+        timeLeftText += seconds;
+        countdownText.setText(timeLeftText);
+
+        float timeRemaining = ((minutes*60f + seconds) / 300f)*100f;
+        int intTimeRemaining = Math.round(timeRemaining);
+        Toast toast = Toast.makeText(getActivity(), "Time remaining is: " + timeRemaining + " seconds", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP|Gravity.RIGHT, 0, 0);
+        toast.show();
+        progressBar.setProgress(intTimeRemaining);
+    }
 
 } //END BOT1FRAG
